1c0953b23e0073d24539f7d0632998bc4d49706e
hayato.wakamatsu <hayato.wakamatsu@trustbrain.co.jp>
2024-05-14 16:41:48 +0900

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import Demo from "./components/Demo"
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>Wirte From Here</h1>
+      <Demo></Demo>
+    </>
+  )
+}
+
+export default App
---
+++ src/renderer/src/components/Demo.tsx
@@ -0,0 +1,9 @@
+function Demo(): JSX.Element {
+  return (
+    <>
+      <button className="button">This is Demo Component</button>
+    </>
+  )
+}
+
+export default Demo
